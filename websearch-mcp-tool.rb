2d3b7d87f6974adeca9fd8b098c1c04df7cdbd06
Andy Moncsek <amo@mailbox.org>
2026-07-31 22:24:21 +0200

    update sha

--- websearch-mcp-tool.rb
+++ websearch-mcp-tool.rb
@@ -2,7 +2,7 @@ class WebsearchMcpTool < Formula
   desc "Lightweight MCP tool for web search and content fetching"
   homepage "https://codeberg.org/amoAHCP/web-mcp"
   url "https://codeberg.org/amoAHCP/web-mcp/releases/download/v1.0/websearch-mcp-tool-macos.tar.gz"
-  sha256 "368b39e0d5cd6ffeb45c7c00c766c6941b8637e3e20f4ceb0a9520634f697a7b"
+  sha256 "e6b74beabe754c674cf79bc2304b6056da4b110f3198add47ad4f5e4e748afd7"
 
   def install
     bin.install "websearch-mcp-tool"
